f6a2a2d6f80738a4eb85de5e06e863327f4178ed
DPEJOVIC <102003169+DPEJOVIC@users.noreply.github.com>
2024-11-03 13:39:33 +1100

    Initial commit.

---
+++ pages/1_Activity_1.py
@@ -0,0 +1,106 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("Activity 1: Difficult Conversations")
+
+
+if "level" not in st.session_state:
+    st.session_state["level"] = ""
+
+if "diffchanged" not in st.session_state:
+    st.session_state["diffchanged"] = False
+
+# Initialise response counter
+if "response_counter" not in st.session_state:
+    st.session_state.response_counter = 0
+
+# Initialise chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Set up OpenAI API client
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "part_1_done" not in st.session_state:
+    st.session_state["part_1_done"] = False
+
+###############
+def diffchange():
+    st.session_state["diffchanged"] = True
+
+option = st.selectbox(label="Select a difficulty level for the conversation:", 
+                        options=("Level 1", "Level 2", "Level 3"),
+                        placeholder="Choose a difficulty level",
+                        disabled=st.session_state["diffchanged"],
+                        on_change=diffchange)
+
+if option:
+    st.session_state["level"] = option
+
+###############
+
+if not st.session_state["diffchanged"]:
+    exit()
+
+# Read system prompt and level
+with open("./prompts/teacherpeerprompt.txt", "r") as file:
+    systemprompt = file.read()
+
+if st.session_state["level"] == "Level 1":
+    with open("./prompts/level1.txt", "r") as file:
+        levelprompt = file.read()
+elif st.session_state["level"] == "Level 2":
+    with open("./prompts/level2.txt", "r") as file:
+        levelprompt = file.read()
+else:
+    with open("./prompts/level3.txt", "r") as file:
+        levelprompt = file.read()
+
+systemprompt = f"{systemprompt} \n\n {levelprompt}"
+
+###############
+
+# Write greeting & chat history
+with st.chat_message("assistant"):
+    st.markdown("Welcome! Don’t worry, this is a safe space for practicing your skills. Things might get heated but just remember its not personal and I’m not real!")
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Chat logic
+if prompt := st.chat_input("Write a response here", disabled = st.session_state.response_counter >= 10):
+
+    st.session_state["part_1_done"] = True
+
+    st.session_state.chat_history.append({"role": "user", "content": prompt})
+
+    if st.session_state.response_counter < 10:
+    
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            messages_with_system_prompt = [{"role": "system", "content": systemprompt}] + [
+                {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.chat_history
+            ]
+
+            stream = client.chat.completions.create(
+                model = st.session_state["openai_model"],
+                messages = messages_with_system_prompt,
+                stream = True,
+            )
+            response = st.write_stream(stream)
+
+        st.session_state.response_counter += 1
+        st.session_state.chat_history.append({"role": "assistant", "content": response})
+    
+    else:
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        
+        with st.chat_message("assistant"):
+            st.markdown("That was intense. I hope you feel like you are really prepared to go have a difficult conversation with a real person! You nailed it!")
+        
+        st.session_state.chat_history.append({"role": "assistant", "content": "That was intense. I hope you feel like you are really prepared to go have a difficult conversation with a real person! You nailed it!"})
---
+++ pages/2_Activity_2.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from openai import OpenAI
+
+st.title("Activity 2: Debrief")
+
+if "part_1_done" not in st.session_state:
+    st.session_state["part_1_done"] = False
+
+if not st.session_state["part_1_done"]:
+    st.write("Please complete Activity 1 first.")
+    exit()
+
+# Initialise response counter
+if "response_counter_2" not in st.session_state:
+    st.session_state.response_counter_2 = 0
+
+# Initialise chat history
+if "chat_history_2" not in st.session_state:
+    st.session_state.chat_history_2 = []
+
+# Set up OpenAI API client
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+###############
+
+# Read system prompt
+with open("./prompts/debrief.txt", "r") as file:
+    systemprompt = file.read()
+
+a1_messages = st.session_state["chat_history"]
+formatted_messages = "\n".join([f"{message['role'].capitalize()}: {message['content']}" for message in a1_messages])
+
+systemprompt = f"{systemprompt} \n\n {formatted_messages}"
+
+###############
+
+# Write greeting & chat history
+with st.chat_message("assistant"):
+    st.markdown("Welcome! Reflection on our practice is essential. It is great you have come here for some feedback. Feedback can be difficult to accept, but it is useful information to help us grow.")
+
+for message in st.session_state.chat_history_2:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Chat logic
+if prompt := st.chat_input("Write a response here", disabled = st.session_state.response_counter_2 >= 10):
+
+    st.session_state["part_1_done"] = True
+    
+    st.session_state.chat_history_2.append({"role": "user", "content": prompt})
+
+    if st.session_state.response_counter_2 < 10:
+    
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant"):
+            messages_with_system_prompt = [{"role": "system", "content": systemprompt}] + [
+                {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.chat_history_2
+            ]
+
+            stream = client.chat.completions.create(
+                model = st.session_state["openai_model"],
+                messages = messages_with_system_prompt,
+                stream = True,
+            )
+            response = st.write_stream(stream)
+
+        st.session_state.response_counter_2 += 1
+        st.session_state.chat_history_2.append({"role": "assistant", "content": response})
+    
+    else:
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        
+        with st.chat_message("assistant"):
+            st.markdown("Well done. Engaging with the feedback will help you improve. Keep up with your practice.")
+        
+        st.session_state.chat_history_2.append({"role": "assistant", "content": "Well done. Engaging with the feedback will help you improve. Keep up with your practice."})
